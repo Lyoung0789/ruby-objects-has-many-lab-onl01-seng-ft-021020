77c25575f9c74d3465d7e69c4cb497244f43b653
Lester Young <lyoung070789@gmail.com>
2020-02-22 23:59:15 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,6 +11,10 @@ class Author
   
   def add_post(new_post)
     new_post.author = self
-    
   end
+  
+  def add_post_by_title(post_title)
+    post_title = Post.new
+    post_title.author = self
+  end 
 end 
